c815292a591f8363812c7e60bde9cb4dbc1e9014
admin <admin@192.168.1.75>
2016-10-06 07:59:17 +0200

    Acceuil client

--- GestibankOceanim/src/main/webapp/static/js/controller/authentiCtrl.js
+++ GestibankOceanim/src/main/webapp/static/js/controller/authentiCtrl.js
@@ -72,7 +72,33 @@ App.controller('AccordionDemoCtrl', function ($scope,$location) {
 	});
 
 
+/*------ De Céline --------*/
 
- 
+App.controller("modifInfo", function($scope) {
+    $scope.showMe = false;
+    $scope.modif = function() {
+     $scope.showMe = !$scope.showMe;
+    }
+});
+
+App.controller('tempCtrl', [
+                    		'$scope',
+                    		'$location',
+                    		'$window',
+                    		'$sce',function($scope, $location, $window, $sce) {
+
+                    			$scope.dynamicPopover = {
+                    				content : 'Hello, World!',
+                    				templateUrl : 'virement.html',// 'myPopoverTemplate.html',//
+                    				title : 'Title'
+                    			};
+                    			$scope.placement = {
+                    				options : [ 'top', 'top-left', 'top-right', 'bottom',
+                    						'bottom-left', 'bottom-right', 'left', 'left-top',
+                    						'left-bottom', 'right', 'right-top', 'right-bottom' ],
+                    				selected : 'right'
+                    			};
+
+                    }]);
 
 
